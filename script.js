cfacf1e472673967970a6f90e6ca2532f577d981
Anastasiya Bazarova <Nassi_13@inbox.ru>
2019-07-09 20:17:10 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+    });
+    
+    $('.menu__links-item').on('click', function() {
+      // do something
+
+      $(this).closest('.menu')
+        .removeClass('menu_state_open');
+    });
+  });
